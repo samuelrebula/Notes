1e9f70b40f6400b46949e20c9396d760d0317617
samuel <rebuuula@gmail.com>
2024-02-06 20:43:15 -0300

    usando map e forEach no JavaScript

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
 
 function App() {
+  //               0 1 2 3 4 5 6 7 8 posição do array
+  let numeros = [1,2,3,4,5,6,7,8,9];
+
+  function clicou() {
+    numeros = numeros.map((elemento) => {
+      return elemento * 2;
+    });
+
+    console.log(numeros)
+    // numeros.forEach((elemento, index) => {
+    //   console.log(elemento + '-' + index)
+    // });
+  }
+
   return (
     <div>
-      Hello world
+      <h1>Hello world</h1>
+      <h1>{numeros}</h1>      
+      <button onClick={() => {clicou(false)}} >Clique aqui</button>
     </div>
   );
 }
