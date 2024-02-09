15a3bd89ad9d2526f019a8c7eb6be0828ac40d4b
samuel <rebuuula@gmail.com>
2024-02-09 00:11:34 -0300

    functional Login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import "./Login.css"
 
 function Login() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const navigate = useNavigate();
+
+    function login() {
+        if(email === "teste@gmail.com" && password === "123"){
+            alert("Welcome\n" + email);
+            navigate('/home');
+        } else {
+            alert("Wrong email or password");
+        }
+    }
+
     return (
         <div className="base">
             <div className="container">
@@ -11,13 +25,21 @@ function Login() {
                     <Form className="inputs">
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             {/* <Form.Label>Email address</Form.Label> */}
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control 
+                                type="email" 
+                                placeholder="Enter email" 
+                                onChange={(e) => {setEmail(e.target.value)}} 
+                            />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             {/* <Form.Label>Password</Form.Label> */}
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control 
+                                type="password" 
+                                placeholder="Password" 
+                                onChange={(e) => {setPassword(e.target.value)}}
+                            />
                         </Form.Group>
-                        <Button variant="primary">Login</Button>
+                        <Button variant="primary" onClick={login}>Login</Button>
                     </Form>
                 </div>
             </div> 
